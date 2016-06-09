7392a7ab244a164991007ffea26b1907ff7c3c0a
Alex Wilhelm <awwilhelm2014@gmail.com>
2016-06-09 10:08:05 -0500

    no message

---
+++ Assets/Editor/BuildScript.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+public class BuildScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+
+    [MenuItem ("Build/BuildWebplayer")]
+    static void MyBuild()
+    {
+        string[] scenes = { "Assets/funville.unity" };
+        BuildPipeline.BuildPlayer(scenes, "Build/name.exe", BuildTarget.StandaloneWindows64, BuildOptions.None);
+
+    }
+
+}
