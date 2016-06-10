690ebc8518cf456174470db9a82cec1d178e4d46
Alex Wilhelm <awwilhelm2014@gmail.com>
2016-06-10 16:29:29 -0500

    modified:   Assets/Editor/BuildScript.cs 	modified:   ProjectSettings/EditorSettings.asset#

--- Assets/Editor/BuildScript.cs
+++ Assets/Editor/BuildScript.cs
@@ -4,23 +4,55 @@ using System.Collections;
 
 public class BuildScript : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    static string[] scenes = {
+        "Assets/funville.unity"
+    };
 
 
-    [MenuItem ("Build/BuildWebplayer")]
-    static void MyBuild()
+    [MenuItem("Build/StandaloneWindows32")]
+    static void StandaloneWindows32()
     {
-        string[] scenes = { "Assets/funville.unity" };
-        BuildPipeline.BuildPlayer(scenes, "Build/name.exe", BuildTarget.StandaloneWindows64, BuildOptions.None);
-
+        BuildPipeline.BuildPlayer(scenes, "Build/StandaloneWindows32/StandaloneWindows32.exe", BuildTarget.StandaloneWindows, BuildOptions.None);
     }
+
+    [MenuItem ("Build/StandaloneWindows64")]
+    static void StandaloneWindows64()
+    {
+        BuildPipeline.BuildPlayer(scenes, "Build/StandaloneWindows64/StandaloneWindows64.exe", BuildTarget.StandaloneWindows64, BuildOptions.None);
+    }
+
+    [MenuItem("Build/StandaloneOSXIntel")]
+    static void StandaloneOSXIntel()
+    {
+        BuildPipeline.BuildPlayer(scenes, "Build/StandaloneOSXIntel/StandaloneOSXIntel.exe", BuildTarget.StandaloneOSXIntel, BuildOptions.None);
+    }
+
+    [MenuItem("Build/StandaloneOSXIntel64")]
+    static void StandaloneOSXIntel64()
+    {
+        BuildPipeline.BuildPlayer(scenes, "Build/StandaloneOSXIntel64/StandaloneOSXIntel64.exe", BuildTarget.StandaloneOSXIntel64, BuildOptions.None);
+    }
+
+    [MenuItem("Build/StandaloneOSXUniversal")]
+    static void StandaloneOSXUniversal()
+    {
+        BuildPipeline.BuildPlayer(scenes, "Build/StandaloneOSXUniversal/StandaloneOSXUniversal.exe", BuildTarget.StandaloneOSXUniversal, BuildOptions.None);
+    }
+
+    [MenuItem("Build/All")]
+    static void All()
+    {
+        //Windows Builds
+        StandaloneWindows32();
+        StandaloneWindows64();
+
+        //Mac Builds
+        //StandaloneOSXIntel();
+        //StandaloneOSXIntel64();
+        //StandaloneOSXUniversal();
+    }
+
+
+
 
 }
